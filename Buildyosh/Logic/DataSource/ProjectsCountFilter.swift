bda7bed97640e4357ed53a14c2352d48d9a99475
Vyacheslav Khorkov <artfintch@yandex.com>
2020-10-27 21:24:22 +0500

    Add others project section.

--- Buildyosh/Logic/DataSource/ProjectsCountFilter.swift
+++ Buildyosh/Logic/DataSource/ProjectsCountFilter.swift
@@ -15,20 +15,31 @@ struct ProjectsCountFilter {
         var sorted = notEmptyDuraton.sorted { $0.duration > $1.duration }
 
         var duration = 0.0
-        while sorted.count > 5 {
+        var count = 0
+        var daysCount = 0
+        var successCount = 0
+        var failCount = 0
+        while sorted.count > 4 {
             let last = sorted.removeLast()
             duration += last.duration
+            count += last.count
+            daysCount += last.daysCount
+            successCount += last.successCount
+            failCount += last.failCount
         }
 
-//        if duration > 0 {
-//            let others = Project(id: "Others",
-//                                 name: "Others",
-//                                 duration: duration,
-//                                 count: 0,
-//                                 daysCount: 0,
-//                                 modifiedDate: Date())
-//            sorted.append(others)
-//        }
+        if duration > 0 {
+            let others = Project(id: "Others_id",
+                                 name: "Others",
+                                 duration: duration,
+                                 count: count,
+                                 daysCount: daysCount,
+                                 modifiedDate: Date(),
+                                 successCount: successCount,
+                                 failCount: failCount,
+                                 dates: [])
+            sorted.append(others)
+        }
 
         return sorted
     }
